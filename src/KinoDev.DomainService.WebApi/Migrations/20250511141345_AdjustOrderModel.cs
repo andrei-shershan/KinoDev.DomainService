e8ea219221486c55a1fd630d07b62ce4e1363f38
Andrei Shershan <139045906+andrei-shershan@users.noreply.github.com>
2025-05-11 17:56:16 +0200

    Email sent (#20)
    * dev
    
    * dev

---
+++ src/KinoDev.DomainService.WebApi/Migrations/20250511141345_AdjustOrderModel.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KinoDev.DomainService.WebApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdjustOrderModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HashCode",
+                table: "Orders");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "EmailSent",
+                table: "Orders",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EmailSent",
+                table: "Orders");
+
+            migrationBuilder.AddColumn<string>(
+                name: "HashCode",
+                table: "Orders",
+                type: "varchar(64)",
+                maxLength: 64,
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
