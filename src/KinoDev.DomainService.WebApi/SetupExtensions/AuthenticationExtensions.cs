8fc2b07bb939a11cfd8a68262184760e8ef53fb4
Andrei Shershan <139045906+andrei-shershan@users.noreply.github.com>
2025-06-24 23:29:48 +0200

    dev (#38)

--- src/KinoDev.DomainService.WebApi/SetupExtensions/AuthenticationExtensions.cs
+++ src/KinoDev.DomainService.WebApi/SetupExtensions/AuthenticationExtensions.cs
@@ -18,7 +18,8 @@ namespace KinoDev.DomainService.WebApi.SetupExtensions
                     options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
                     options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
                 })
-                .AddJwtBearer(options => {
+                .AddJwtBearer(options =>
+                {
                     options.TokenValidationParameters = new TokenValidationParameters
                     {
                         ValidateIssuer = true,
@@ -29,6 +30,19 @@ namespace KinoDev.DomainService.WebApi.SetupExtensions
                         ValidAudience = authenticationSettings.Audiences.Internal,
                         IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(authenticationSettings.Secret)),
                         ClockSkew = TimeSpan.Zero
+                    };
+
+                    options.RequireHttpsMetadata = true; // Set to true in production
+
+                    // Add this for Azure deployment
+                    options.Events = new JwtBearerEvents
+                    {
+                        OnAuthenticationFailed = context =>
+                        {
+                            // Log the error for debugging
+                            Console.WriteLine($"Authentication failed: {context.Exception}");
+                            return Task.CompletedTask;
+                        }
                     };
                 });
 
